6bf10b4b65b054dfc1d0d60061d933ae48e421c9
Ayoade Clinton <clintonayomide96@example.com>
2025-07-22 14:34:28 +0100

    adjustments

--- crownlinks/app/assets/icons.js
+++ crownlinks/app/assets/icons.js
@@ -1,12 +1,15 @@
 import dropdown from './icons/chevron-down.png'
 import cart from './icons/shopping-cart-03.png'
 import meta_cirle from './icons/Ellipse 541.png'
+import arrow_right from './icons/chevron-right.png'
+import arrow_left from './icons/chevron-left.png'
 
 const icons ={
     dropdown,
     cart,
     meta_cirle,
-    
+    arrow_right,
+    arrow_left
 }
 
 export default icons;
